547c07c69182bc656c229e8b0e6e6433946d739f
kev <kevin24crypto@gmail.com>
2018-10-26 16:49:08 +0530

    updating full project to git

--- src/main/java/Liscense/ComputerIdentifier.java
+++ src/main/java/Liscense/ComputerIdentifier.java
@@ -3,6 +3,7 @@ package Liscense;
      import oshi.SystemInfo;
      import oshi.hardware.CentralProcessor;
      import oshi.hardware.ComputerSystem;
+     import oshi.hardware.HWDiskStore;
      import oshi.hardware.HardwareAbstractionLayer;
      import oshi.software.os.OperatingSystem;
 
@@ -10,16 +11,21 @@ package Liscense;
      {
      static String generateLicenseKey()
      {
+          String diskSerialID="";
      SystemInfo systemInfo = new SystemInfo();
      OperatingSystem operatingSystem = systemInfo.getOperatingSystem();
      HardwareAbstractionLayer hardwareAbstractionLayer = systemInfo.getHardware();
      CentralProcessor centralProcessor = hardwareAbstractionLayer.getProcessor();
      ComputerSystem computerSystem = hardwareAbstractionLayer.getComputerSystem();
-
+          HWDiskStore[]  HWDStore = hardwareAbstractionLayer.getDiskStores();
+          for(HWDiskStore disk : HWDStore) {
+               diskSerialID = disk.getSerial().trim();
+          }
      String vendor = operatingSystem.getManufacturer();
-     String processorSerialNumber = computerSystem.getSerialNumber();
-     String processorIdentifier = centralProcessor.getIdentifier();
-     int processors = centralProcessor.getLogicalProcessorCount();
+     String processorSerialNumber = computerSystem.getSerialNumber(); //processor serialNumber is Bios serialNumber
+     String processorIdentifier = centralProcessor.getProcessorID();  //processorID is same as CPUID
+     String motherBoardIdentifier = computerSystem.getBaseboard().getSerialNumber();
+     int processors = centralProcessor.getLogicalProcessorCount(); //No of cores like logical cpu count for this machine
 
      String delimiter = "#";
 
@@ -29,7 +35,9 @@ package Liscense;
      delimiter +
      processorIdentifier +
      delimiter +
-     processors;
+     processors +
+     delimiter +
+     motherBoardIdentifier + delimiter + diskSerialID;
      }
 
      public static void main(String[] arguments)
